6bd42159c8d2cbc80072a782b10f546aa0ef7323
Sunil Malik <kumar85sunil@gmail.com>
2022-05-07 10:43:24 +0530

    first commit

---
+++ app/src/main/java/com/example/myretrofit/RetrofitAPI.java
@@ -0,0 +1,12 @@
+package com.example.myretrofit;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface RetrofitAPI {
+
+    @POST("users")
+    Call<DataModal> createPost(@Body DataModal dataModal);
+
+}
